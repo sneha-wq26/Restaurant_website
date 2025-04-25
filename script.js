20a583e2c6be3dd6315081938bf6d93570201e2e
sneha-wq26 <snehazee54@gmail.com>
2025-04-25 10:22:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Simple JS Example - Form Submit
+document.querySelector("form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Thank you! We'll contact you soon.");
+  });
+  
